e694b20d70b0c7dd67f4b0a782926c5f49b99b73
Asanga <cspl.itm@googlemail.com>
2017-11-13 16:55:34 +0530

    Your Message

--- TestingApp/src/app/app.component.ts
+++ TestingApp/src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: '[app-root]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  private jobtitle ="Software Engineer";
+  job:object;
+  isDisabled = true;
+
+  possition:string;
+  Address:string;
+  Area:string;
+
+  //Css class names
+  specialh3 = "special-h3";
+  spcecialcase = "spcecial-case";
+
+  singleLine = "alert alert-danger";
+  ischange = false;
+
+  alertstyles ={
+    "alert" :true,
+    "alert-success" :true
+  }  
+
+  getContact(){
+    return "+94 778151151";
+  }
+
+  onCLickmybtn(){
+    alert("You clicked me!");
+    this.possition = "Software Programmer";
+  }
+
+  onCLickmybtn2(event:any){
+    alert("You clicked me!" + event.target.textContent);
+    this.Address = "270/A Mullegama ,Homagama";
+
+  }
+
+  buttonCLickColorCHange(val:any){
+    
+      if(val ===1){
+        alert("Incoming");
+        this.ischange =true;
+      }else{
+        this.ischange =false;
+      }
+  }
+
 }
